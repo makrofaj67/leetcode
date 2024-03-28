64a36ec08b64eda2218e461dbe4482b3657165ba
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-28 22:48:53 +0300

    twosumpy

---
+++ Python/1_Two_Sum/twosum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                j += 1
+            i += 1
