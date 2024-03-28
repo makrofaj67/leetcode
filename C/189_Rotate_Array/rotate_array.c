bd2ae9c8fea00b8d20f8fa054946894a52350c61
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-28 22:26:25 +0300

    rotate_array

--- C/189_Rotate_Array/rotate_array.c
+++ C/189_Rotate_Array/rotate_array.c
@@ -6,7 +6,7 @@
 /*   By: rakman <rakman@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 21:25:21 by rakman            #+#    #+#             */
-/*   Updated: 2024/03/28 21:52:21 by rakman           ###   ########.fr       */
+/*   Updated: 2024/03/28 22:24:50 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,30 @@ void	rotate(int *nums, int numsSize, int k)
 	int	i;
 	int	j;
 
-	k = k % numsSize;
-	j = 0;
-	i = k;
-	temp = (int *)malloc(sizeof(int) * numsSize);
-	while (i <= numsSize - 1)
+	if (numsSize != 1)
 	{
-		temp[j] = nums[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (i < k)
-	{
-		temp[j] = nums[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (i < numsSize)
-	{
-		nums[i] = temp[i];
-		i++;
+		k = k % numsSize;
+		i = numsSize - k;
+		j = 0;
+		temp = (int *)malloc(sizeof(int) * numsSize);
+		while (i < numsSize)
+		{
+			temp[j] = nums[i];
+			i++;
+			j++;
+		}
+		i = 0;
+		while (i < numsSize - k)
+		{
+			temp[j] = nums[i];
+			i++;
+			j++;
+		}
+		i = 0;
+		while (i < numsSize)
+		{
+			nums[i] = temp[i];
+			i++;
+		}
 	}
 }
